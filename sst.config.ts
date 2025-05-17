b1ff2f371eebc2b3b38c71ee0bfa6e62ec4c3efd
kkniest <k@khsk.net>
2025-05-16 21:45:13 -0700

    deploy with sst

--- sst.config.ts
+++ sst.config.ts
@@ -27,6 +27,8 @@ export default $config({
       },
       domain: {
         name: 'stats.bigskybuyers.com',
+        dns: false,
+        cert: 'arn:aws:acm:us-east-1:758722857241:certificate/0677a522-1283-4c1f-9abc-d9e2497ed543',
       },
     });
   },
