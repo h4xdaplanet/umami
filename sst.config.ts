9b7115a84bb0bbf0bd936a5d8891fcbf44669840
kkniest <k@khsk.net>
2025-05-16 21:45:13 -0700

    deploy with sst

---
+++ sst.config.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: 'umami',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
+    };
+  },
+  async run() {
+    const vpc = sst.aws.Vpc.get('BigskyVpc', 'vpc-03c527b561aff09b8');
+    const rds = sst.aws.Postgres.get('BigskyDb', {
+      id: 'bigskybuyerswebsite-dev-bigskydbinstance',
+    });
+    const rdsSchema = 'umami';
+    const rdsUrl = $interpolate`postgresql://${rds.username}:${rds.password}@${rds.host}:${rds.port}/${rds.database}?schema=${rdsSchema}`;
+
+    new sst.aws.Nextjs('bigsky-umami', {
+      vpc: vpc,
+      link: [rds],
+      environment: {
+        DATABASE_URL: rdsUrl,
+      },
+      domain: {
+        name: 'stats.bigskybuyers.com',
+        dns: false,
+        cert: 'arn:aws:acm:us-east-1:758722857241:certificate/0677a522-1283-4c1f-9abc-d9e2497ed543',
+      },
+    });
+  },
+});
